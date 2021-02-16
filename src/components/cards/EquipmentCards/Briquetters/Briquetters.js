48ae315da2a44ee4510392a0ea79dc0031266ea7
Mitchell Wilson <mitchellwilson01@outlook.com>
2021-02-16 17:23:36 -0600

    possibly fixed nav links error after deployment

---
+++ src/components/cards/EquipmentCards/Briquetters/Briquetters.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import './Briquetters.scss';
+
+const BeltPresses = () => {
+    return (
+        <>
+            <div className="belt-presses-container">
+                <Card className="belt-presses-card">Hi</Card>
+                <Card className="belt-presses-card">Hi</Card>
+                <Card className="belt-presses-card">Hi</Card>
+                <Card className="belt-presses-card">Hi</Card>
+                <Card className="belt-presses-card">Hi</Card>
+                <Card className="belt-presses-card">Hi</Card>
+                <Card className="belt-presses-card">Hi</Card>
+            </div>
+        </>
+    );
+}
+
+export default BeltPresses;
--- src/components/pages/Equipment/Equipment.js
+++ src/components/pages/Equipment/Equipment.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { ButtonGroup, Button } from 'react-bootstrap';
+import Briquetters from '../../cards/EquipmentCards/Briquetters/Briquetters';
+import './Equipment.scss';
 
 
 const Equipment = () => {
     return (
         <>
-            <h1>Equipment Page</h1>
+            <div className="equipment-briquetters-title">BRIQUETTERS</div>
+            <Briquetters />
         </>
     );
 }
 
 export default Equipment;
+
+/*
+<ButtonGroup aria-label="Basic example" className="equipment-buttons">
+                    <Button variant="secondary">Briquetters</Button>
+                    <Button variant="secondary">Shredder</Button>
+                    <Button variant="secondary">Conveyor</Button>
+                    <Button variant="secondary">Layout</Button>
+                </ButtonGroup>
+*/
